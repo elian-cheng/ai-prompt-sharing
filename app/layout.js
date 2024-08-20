000b1932cc666bb408d48fef51ca8fa344da220f
Olga Chernega <eliang.cheng@gmail.com>
2024-08-20 14:30:20 +0300

    init: start project ai-prompt-sharing, add layout and dependencies

---
+++ app/layout.js
@@ -0,0 +1,20 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "Promptopia",
+  description: "Discover & Share AI Prompts"
+};
+
+const RootLayout = ({ children }) => (
+  <html lang="en">
+    <body>
+      <div className="main">
+        <div className="gradient" />
+      </div>
+
+      <main className="app">{children}</main>
+    </body>
+  </html>
+);
+
+export default RootLayout;
